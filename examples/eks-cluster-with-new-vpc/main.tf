699c1b701122fa09554a6774a71a31e99db0dacc
jrobinsonvm <73367284+jrobinsonvm@users.noreply.github.com>
2022-08-29 17:22:11 -0400

    Update main.tf

--- examples/eks-cluster-with-new-vpc/main.tf
+++ examples/eks-cluster-with-new-vpc/main.tf
@@ -30,8 +30,9 @@ locals {
   region       = "us-east-2"
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-  
+#  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+
   tags = {
     Blueprint  = local.name
     GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
