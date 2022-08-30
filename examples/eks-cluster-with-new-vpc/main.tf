4d009e1c892d70a1cea3b8a11fcc817c96a5bfba
jrobinsonvm <73367284+jrobinsonvm@users.noreply.github.com>
2022-08-29 23:29:17 -0400

    Update main.tf

--- examples/eks-cluster-with-new-vpc/main.tf
+++ examples/eks-cluster-with-new-vpc/main.tf
@@ -30,7 +30,7 @@ data "aws_eks_cluster_auth" "this" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name = "eks-lab-amer-dev"
+  name = "eks-lab-amer-01"
 #  name = basename(path.cwd)
   # var.cluster_name is for Terratest
   cluster_name = coalesce(var.cluster_name, local.name)
